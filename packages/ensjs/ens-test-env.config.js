c8fb4fa0b213d73c9d099af44ad6d52e65ddface
TateB <yo@taytems.xyz>
2022-04-28 11:01:28 +1000

    strongly typed env config

--- packages/ensjs/ens-test-env.config.js
+++ packages/ensjs/ens-test-env.config.js
@@ -1,19 +1,28 @@
 require('dotenv').config({ path: process.env.INIT_CWD + '/.env' })
 
+/**
+ * @type {import('@ensdomains/ens-test-env').ENSTestEnvConfig}
+ **/
 module.exports = {
-  docker: {
-    chainId: 3,
-    network: 'ropsten',
-    forkRpcUrl: process.env.FORK_RPC_URL,
-    secretWords: 'test test test test test test test test test test test junk',
-    unlockedAccounts: ['0xa303ddC620aa7d1390BACcc8A495508B183fab59'],
-  },
+  deployCommand: 'yarn hardhat deploy',
   archive: {
     subgraphId: 'QmXxAE7Urtv6TPa8o8XmPwLVQNbH6r35hRKHP63udTxTNa',
     epochTime: 1646894980,
-    block: 12066620,
+    blockNumber: 12066620,
     baseUrl: 'https://storage.googleapis.com/ens-manager-build-data',
+    network: 'ropsten',
   },
-  deployCommand: 'yarn hardhat deploy',
+  ethereum: {
+    chain: {
+      chainId: 3,
+    },
+    fork: {
+      url: process.env.FORK_RPC_URL,
+    },
+    wallet: {
+      mnemonic: 'test test test test test test test test test test test junk',
+      unlockedAccounts: ['0xa303ddC620aa7d1390BACcc8A495508B183fab59'],
+    },
+  },
   scripts: [],
 }
