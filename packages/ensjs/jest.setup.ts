18d02d7cc2577eb5b025a1017f7306cb858224c6
tate <tate@ens.domains>
2022-06-02 15:52:55 +1000

    commitment utils

--- packages/ensjs/jest.setup.ts
+++ packages/ensjs/jest.setup.ts
@@ -1,1 +1,8 @@
+import crypto from 'crypto'
 import 'jest-localstorage-mock'
+
+Object.defineProperty(global, 'crypto', {
+  value: {
+    getRandomValues: (arr: any) => crypto.randomBytes(arr.length),
+  },
+})
