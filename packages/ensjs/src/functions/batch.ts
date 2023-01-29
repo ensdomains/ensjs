47a3ba8501ea4ead95f5fed189d86335bbd9e154
tate <tate@ens.domains>
2023-01-30 10:20:03 +1100

    fixed batch ccip

--- packages/ensjs/src/functions/batch.ts
+++ packages/ensjs/src/functions/batch.ts
@@ -1,3 +1,4 @@
+import { TransactionRequest } from '@ethersproject/abstract-provider'
 import { BatchFunctionResult, ENSArgs, RawFunction } from '..'
 
 const raw = async (
@@ -12,21 +13,30 @@ const raw = async (
       return rawRef(...args)
     }),
   )
-  return multicallWrapper.raw(rawDataArr)
+  const response = await multicallWrapper.raw(rawDataArr)
+  return { ...response, passthrough: rawDataArr }
 }
 
 const decode = async (
   { multicallWrapper }: ENSArgs<'multicallWrapper'>,
   data: string,
+  passthrough: TransactionRequest & { passthrough?: any }[],
   ...items: BatchFunctionResult<RawFunction>[]
 ): Promise<any[] | undefined> => {
-  const response = await multicallWrapper.decode(data)
+  const response = await multicallWrapper.decode(data, passthrough)
   if (!response) return
 
   return Promise.all(
-    response.map((ret: any, i: number) =>
-      items[i].decode(ret.returnData, ...items[i].args),
-    ),
+    response.map((ret: any, i: number) => {
+      if (passthrough[i].passthrough) {
+        return items[i].decode(
+          ret.returnData,
+          passthrough[i].passthrough,
+          ...items[i].args,
+        )
+      }
+      return items[i].decode(ret.returnData, ...items[i].args)
+    }),
   )
 }
 
