7a7b6339fdaffd4233e7ba5f2760536d6a4376e8
tate <tate@ens.domains>
2023-04-12 11:16:31 +1000

    fix: allow setting empty contenthash

--- packages/ensjs/src/utils/recordHelpers.test.ts
+++ packages/ensjs/src/utils/recordHelpers.test.ts
@@ -1,6 +1,6 @@
 import { PublicResolver__factory } from '../generated'
 import { namehash } from './normalise'
-import { generateSetAddr } from './recordHelpers'
+import { generateRecordCallArray, generateSetAddr } from './recordHelpers'
 
 describe('generateSetAddr()', () => {
   it('should allow empty string as address', () => {
@@ -15,5 +15,22 @@ describe('generateSetAddr()', () => {
         ),
       ),
     ).not.toThrowError()
+  })
+})
+
+describe('generateRecordCallArray()', () => {
+  it('should allow empty string as contenthash', () => {
+    expect(
+      generateRecordCallArray(
+        namehash('test'),
+        { contentHash: '' },
+        PublicResolver__factory.connect(
+          '0x0000000000000000000000000000000000000000',
+          undefined as any,
+        ),
+      ),
+    ).toEqual([
+      '0x304e6ade04f740db81dc36c853ab4205bddd785f46e79ccedca351fc6dfcbd8cc9a33dd600000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000',
+    ])
   })
 })
