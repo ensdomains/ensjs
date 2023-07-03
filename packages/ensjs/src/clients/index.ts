ce0fc8278213a8046451ea2b25dcc905656817d3
tate <tate@ens.domains>
2023-07-03 14:10:42 +1000

    added exports

---
+++ packages/ensjs/src/clients/index.ts
@@ -0,0 +1,4 @@
+export * from './decorators'
+export * from './public'
+export * from './subgraph'
+export * from './wallet'
--- packages/ensjs/src/index.ts
+++ packages/ensjs/src/index.ts
@@ -1,3 +1,4 @@
+export * from './clients'
 export { addEnsContracts, getChainContractAddress } from './contracts'
 export * from './functions'
 export * from './utils'
