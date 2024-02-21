c4ad284e2e5eab7345c9fffb99d69c3f36fb4bcc
tate <tate@ens.domains>
2024-02-22 10:05:24 +1100

    enable coverage

--- packages/ensjs/vitest.config.ts
+++ packages/ensjs/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      enabled: true,
+      include: ['src/**/*'],
+    },
     environment: 'happy-dom',
     setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.test.ts'],
