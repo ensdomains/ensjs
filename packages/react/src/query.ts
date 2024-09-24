9a6c8571afedd0b08ae3f04c62f561181cc0866a
tate <tate@ens.domains>
2024-09-24 12:21:11 +1000

    added remaining public funcs + package.json updates

--- packages/react/src/query.ts
+++ packages/react/src/query.ts
@@ -1,4 +1,132 @@
-import { QueryClient } from '@tanstack/react-query'
-
-// TODO: figure out why not taking from provider
-export const fallbackQueryClient = new QueryClient()
+export {
+  getDecodedNameQueryKey,
+  getDecodedNameQueryOptions,
+  type GetDecodedNameData,
+  type GetDecodedNameErrorType,
+  type GetDecodedNameOptions,
+  type GetDecodedNameParameters,
+  type GetDecodedNameQueryFnData,
+  type GetDecodedNameQueryKey,
+  type GetDecodedNameReturnType,
+} from './query/getDecodedName.js'
+export {
+  getEnsAbiQueryOptions,
+  type GetEnsAbiData,
+  type GetEnsAbiErrorType,
+  type GetEnsAbiOptions,
+  type GetEnsAbiParameters,
+  type GetEnsAbiQueryFnData,
+  type GetEnsAbiQueryKey,
+  type GetEnsAbiReturnType,
+} from './query/getEnsAbi.js'
+export {
+  getEnsAddressQueryOptions,
+  type GetEnsAddressData,
+  type GetEnsAddressErrorType,
+  type GetEnsAddressOptions,
+  type GetEnsAddressParameters,
+  type GetEnsAddressQueryFnData,
+  type GetEnsAddressQueryKey,
+  type GetEnsAddressReturnType,
+} from './query/getEnsAddress.js'
+export {
+  getEnsAvailableQueryOptions,
+  type GetEnsAvailableData,
+  type GetEnsAvailableErrorType,
+  type GetEnsAvailableOptions,
+  type GetEnsAvailableParameters,
+  type GetEnsAvailableQueryFnData,
+  type GetEnsAvailableQueryKey,
+  type GetEnsAvailableReturnType,
+} from './query/getEnsAvailable.js'
+export {
+  getEnsContentHashQueryOptions,
+  type GetEnsContentHashData,
+  type GetEnsContentHashErrorType,
+  type GetEnsContentHashOptions,
+  type GetEnsContentHashParameters,
+  type GetEnsContentHashQueryFnData,
+  type GetEnsContentHashQueryKey,
+  type GetEnsContentHashReturnType,
+} from './query/getEnsContentHash.js'
+export {
+  getEnsCredentialsQueryOptions,
+  type GetEnsCredentialsData,
+  type GetEnsCredentialsErrorType,
+  type GetEnsCredentialsOptions,
+  type GetEnsCredentialsParameters,
+  type GetEnsCredentialsQueryFnData,
+  type GetEnsCredentialsQueryKey,
+  type GetEnsCredentialsReturnType,
+} from './query/getEnsCredentials.js'
+export {
+  getEnsExpiryQueryOptions,
+  type GetEnsExpiryData,
+  type GetEnsExpiryErrorType,
+  type GetEnsExpiryOptions,
+  type GetEnsExpiryParameters,
+  type GetEnsExpiryQueryFnData,
+  type GetEnsExpiryQueryKey,
+  type GetEnsExpiryReturnType,
+} from './query/getEnsExpiry.js'
+export {
+  getEnsNamesForAddressQueryOptions,
+  type GetEnsNamesForAddressData,
+  type GetEnsNamesForAddressErrorType,
+  type GetEnsNamesForAddressOptions,
+  type GetEnsNamesForAddressParameters,
+  type GetEnsNamesForAddressQueryFnData,
+  type GetEnsNamesForAddressQueryKey,
+  type GetEnsNamesForAddressReturnType,
+} from './query/getEnsNamesForAddress.js'
+export {
+  getEnsOwnerQueryOptions,
+  type GetEnsOwnerData,
+  type GetEnsOwnerErrorType,
+  type GetEnsOwnerOptions,
+  type GetEnsOwnerParameters,
+  type GetEnsOwnerQueryFnData,
+  type GetEnsOwnerQueryKey,
+  type GetEnsOwnerReturnType,
+} from './query/getEnsOwner.js'
+export {
+  getEnsPriceQueryOptions,
+  type GetEnsPriceData,
+  type GetEnsPriceErrorType,
+  type GetEnsPriceOptions,
+  type GetEnsPriceParameters,
+  type GetEnsPriceQueryFnData,
+  type GetEnsPriceQueryKey,
+  type GetEnsPriceReturnType,
+} from './query/getEnsPrice.js'
+export {
+  getEnsRecordsQueryOptions,
+  type GetEnsRecordsData,
+  type GetEnsRecordsErrorType,
+  type GetEnsRecordsOptions,
+  type GetEnsRecordsParameters,
+  type GetEnsRecordsQueryFnData,
+  type GetEnsRecordsQueryKey,
+  type GetEnsRecordsReturnType,
+} from './query/getEnsRecords.js'
+export {
+  getEnsResolverInterfacesQueryOptions,
+  type GetEnsResolverInterfacesData,
+  type GetEnsResolverInterfacesErrorType,
+  type GetEnsResolverInterfacesOptions,
+  type GetEnsResolverInterfacesParameters,
+  type GetEnsResolverInterfacesQueryFnData,
+  type GetEnsResolverInterfacesQueryKey,
+  type GetEnsResolverInterfacesReturnType,
+} from './query/getEnsResolverInterfaces.js'
+export {
+  writeEnsRecords,
+  writeEnsRecordsMutationOptions,
+  type WriteEnsRecordsData,
+  type WriteEnsRecordsErrorType,
+  type WriteEnsRecordsMutate,
+  type WriteEnsRecordsMutateAsync,
+  type WriteEnsRecordsParameters,
+  type WriteEnsRecordsReturnType,
+  type WriteEnsRecordsVariables,
+} from './query/writeEnsRecords.js'
