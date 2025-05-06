d8a883f4578b1c484a2c7ca3f81f819aa1167721
v1rtl <hi@v1rtl.site>
2025-05-07 01:23:07 +0300

    simplify build (ditch cjs)

--- packages/ensjs/src/errors/base.ts
+++ packages/ensjs/src/errors/base.ts
@@ -24,6 +24,8 @@ export class BaseError extends Error {
 
   version = getVersion()
 
+  cause?: BaseError | Error
+
   constructor(shortMesage: string, args: BaseErrorParameters = {}) {
     super()
 
