7cdb716f80f757f893e342dd82e719f74140e314
v1rtl <hi@v1rtl.site>
2025-06-02 20:53:25 +0300

    fix tenv config

--- packages/ensjs/ens-test-env.config.js
+++ packages/ensjs/ens-test-env.config.js
@@ -5,10 +5,6 @@ config({
   path: `${process.env.INIT_CWD}/.env`,
   override: true,
 })
-config({
-  path: `${process.env.INIT_CWD}/.env.development.local`,
-  override: true,
-})
 
 process.env.ADDRESS_ETH_REGISTRAR = '0xc5a5C42992dECbae36851359345FE25997F5C42d'
 process.env.ADDRESS_NAME_WRAPPER = '0x9E545E3C0baAB3E08CdfD552C960A1050f373042'
@@ -18,29 +14,16 @@ process.env.BATCH_GATEWAY_URLS = JSON.stringify([
 
 /**
  * @type {import('@ensdomains/ens-test-env').ENSTestEnvConfig}
- **/
+ * */
 export default {
-  deployCommand: 'pnpm hh --network localhost deploy --no-compile',
-  buildCommand: 'pnpm build:glocal && pnpm export',
+  deployCommand: 'pnpm hardhat deploy',
   scripts: [
     {
-      command: 'pnpm wrangle',
-      name: 'wrangler',
-      prefixColor: 'magenta.bold',
-    },
-    {
-      command: `pnpm wait-on http://localhost:8788 && ${
-        process.env.CI
-          ? `pnpm playwright test --project=stateless --shard=${process.env.PLAYWRIGHT_SHARD}/${process.env.PLAYWRIGHT_TOTAL}`
-          : 'pnpm playwright'
-      }`,
-      name: 'playwright',
+      command:
+        process.env.STATIC_ENS === 'true' ? 'pnpm test:static' : 'pnpm test',
+      name: 'vitest',
       prefixColor: 'yellow.bold',
-      env: process.env,
       finishOnExit: true,
     },
   ],
-  paths: {
-    data: './data',
-  },
 }
