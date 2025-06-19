f9979049251f3619aa318e2bd3385e6b19b8b58f
v1rtl <hi@v1rtl.site>
2025-06-19 04:26:02 +0300

    fix getContentHashReord

--- packages/ensjs/src/functions/public/getContentHashRecord.test.ts
+++ packages/ensjs/src/functions/public/getContentHashRecord.test.ts
@@ -1,7 +1,9 @@
-import { RawContractError } from 'viem'
+import { RawContractError, bytesToHex, encodeErrorResult } from 'viem'
 import { describe, expect, it } from 'vitest'
 import type { ClientWithEns } from '../../contracts/consts.js'
+import { universalResolverErrors } from '../../contracts/universalResolver.js'
 import { publicClient } from '../../test/addTestContracts.js'
+import { packetToBytes } from '../../utils/hexEncodedName.js'
 import getContentHashRecord from './getContentHashRecord.js'
 
 describe('getContentHashRecord', () => {
@@ -48,7 +50,11 @@ describe('getContentHashRecord', () => {
       getContentHashRecord.decode(
         {} as ClientWithEns,
         new RawContractError({
-          data: '0x77209fe8', // ResolverNotFound()
+          data: encodeErrorResult({
+            abi: universalResolverErrors,
+            errorName: 'ResolverNotFound',
+            args: [bytesToHex(packetToBytes('test.eth'))],
+          }),
         }),
         {
           address: '0x1234567890abcdef',
@@ -60,7 +66,8 @@ describe('getContentHashRecord', () => {
     ).rejects.toThrowErrorMatchingInlineSnapshot(`
       [ContractFunctionExecutionError: The contract function "resolve" reverted.
 
-      Error: ResolverNotFound()
+      Error: ResolverNotFound(bytes name)
+                             (0x04746573740365746800)
        
       Contract Call:
         address:   0x1234567890abcdef
