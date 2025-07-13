6eabc89f41a6572f33968bf38fed3aaa9cfe8a30
v1rtl <hi@v1rtl.site>
2025-07-13 20:51:05 +0300

    fix description

--- packages/ensjs/src/utils/errors/isReverseAddressMismatch.ts
+++ packages/ensjs/src/utils/errors/isReverseAddressMismatch.ts
@@ -1,8 +1,7 @@
 import { BaseError, ContractFunctionRevertedError } from 'viem'
 
 /**
- * Checks if the data returned from a universal resolver is safe to use, or if it is a known revert error
- * @returns `true` if the data is safe to use, `false` if it is a known revert error, or throws if it is an unknown error
+ * Determines whether the error is a `ReverseAddressMismatch` and returns its arguments if so.
  */
 export const isReverseAddressMismatch = (err: unknown) => {
   if (!(err instanceof BaseError)) return false
