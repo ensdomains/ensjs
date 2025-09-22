00dd12207b22759abaf1ead4e8db9eabb67366e5
storywithoutend <storywithoutend@gmail.com>
2025-09-22 10:05:56 +0800

    Temporarily disable subgraph function tests

--- packages/ensjs/vitest.config.ts
+++ packages/ensjs/vitest.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vitest/config'
+// TODO: Include subgraph functions before final merge.
 
 export default defineConfig({
   test: {
     coverage: {
       enabled: true,
       include: ['src/**/*'],
-      exclude: ['data/**/*'],
+      exclude: ['data/**/*', 'src/functions/subgraph/**/*'],
     },
     environment: 'happy-dom',
     setupFiles: ['./src/test/setup.ts'],
     include: ['src/**/*.test.ts'],
-    exclude: ['data/**/*'],
+    exclude: ['data/**/*', 'src/functions/subgraph/**/*'],
   },
 })
