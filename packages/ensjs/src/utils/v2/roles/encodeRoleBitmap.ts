55feb34461ca9526fd35f28bece19e8c7b3f668e
v1rtl <hi@v1rtl.site>
2025-12-06 20:48:37 +0200

    refactor(ensjs/utils/v2): compact `encodeRoleBitmap`

--- packages/ensjs/src/utils/v2/roles/encodeRoleBitmap.ts
+++ packages/ensjs/src/utils/v2/roles/encodeRoleBitmap.ts
@@ -2,13 +2,6 @@ import * as registryRoles from './registryRoles.js'
 
 export type Role = keyof typeof registryRoles
 
-export const encodeRoleBitmap = (roleList: Role[]): bigint => {
-  let bitmap = 0n
-
-  for (const role of roleList) {
-    // biome-ignore lint: roles are statically defined
-    bitmap |= BigInt(registryRoles[role])
-  }
-
-  return bitmap
-}
+export const encodeRoleBitmap = (roleList: Role[]): bigint =>
+  // biome-ignore lint: all registry roles are statically defined
+  roleList.reduce((a, x) => a || registryRoles[x], 0n)
